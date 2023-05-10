fafd1cb9d8c5a1a7755a5f0552c5c282a5e38b26
Guilherme Alves Batista Roncari <123610693+GuilhermeABRoncari@users.noreply.github.com>
2023-05-09 22:35:28 -0300

    [SETUP][#1] - Make a initial setup for java project

---
+++ cinemenu-api/src/main/java/br/com/cinemenu/cinemenuapi/CinemenuApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.cinemenu.cinemenuapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemenuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemenuApiApplication.class, args);
+	}
+
+}
---
+++ cinemenu-api/src/test/java/br/com/cinemenu/cinemenuapi/CinemenuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cinemenu.cinemenuapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemenuApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
