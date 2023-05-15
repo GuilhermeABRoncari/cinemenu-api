949baf4377f1b19fc608205c2fa65ac61982e845
Guilherme Alves Batista Roncari <123610693+GuilhermeABRoncari@users.noreply.github.com>
2023-05-15 20:21:01 -0300

    [TEST][#19] - Add github secrets to yml

--- src/test/java/br/com/cinemenu/cinemenuapi/CineMenuApiApplicationTest.java
+++ src/test/java/br/com/cinemenu/cinemenuapi/CineMenuApiApplicationTest.java
@@ -3,7 +3,7 @@ package br.com.cinemenu.cinemenuapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CineMenuApiApplicationTest {
