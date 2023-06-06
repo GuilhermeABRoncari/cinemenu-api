b858f73f6fc7ea72f876e45755744c0bb73582b5
Guilherme Alves Batista Roncari <123610693+GuilhermeABRoncari@users.noreply.github.com>
2023-06-06 00:17:11 -0300

    [FEATURE][#23] - Fixing search bug of query parameters

--- src/main/java/br/com/cinemenu/cinemenuapi/rest/controller/CineMenuPreviewMediaController.java
+++ src/main/java/br/com/cinemenu/cinemenuapi/rest/controller/CineMenuPreviewMediaController.java
@@ -5,8 +5,8 @@ import br.com.cinemenu.cinemenuapi.rest.service.PreviewMediaService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,8 +18,9 @@ public class CineMenuPreviewMediaController {
 
     private final PreviewMediaService service;
 
-    @GetMapping("/search?q={search}&page={page}")
-    public ResponseEntity<List<CineMenuMediaResponse>> searchMedia(@PathVariable String search, @PathVariable int page) {
+    @GetMapping("/search")
+    public ResponseEntity<List<CineMenuMediaResponse>> searchMedia(@RequestParam("q") String search, @RequestParam("page") Integer page) {
+        System.out.println(search);
         return ResponseEntity.ok(service.getResponse(search, page));
     }
 
