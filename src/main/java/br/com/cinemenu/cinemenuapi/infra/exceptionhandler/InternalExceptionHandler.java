f475473b2bc45682f415cdde0e902343e49b3804
Guilherme Alves Batista Roncari <123610693+GuilhermeABRoncari@users.noreply.github.com>
2023-07-03 21:17:40 -0300

    [FEATURE][#27] - Add constant in exception handler

--- src/main/java/br/com/cinemenu/cinemenuapi/infra/exceptionhandler/InternalExceptionHandler.java
+++ src/main/java/br/com/cinemenu/cinemenuapi/infra/exceptionhandler/InternalExceptionHandler.java
@@ -4,6 +4,7 @@ import br.com.cinemenu.cinemenuapi.infra.exceptionhandler.exception.InvalidApiKe
 import br.com.cinemenu.cinemenuapi.infra.exceptionhandler.exception.InvalidSearchException;
 import br.com.cinemenu.cinemenuapi.infra.exceptionhandler.exception.TMDBNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -37,7 +38,7 @@ public class InternalExceptionHandler {
 
     @ExceptionHandler(TMDBNotFoundException.class)
     public ResponseEntity notFoundResponseFromTMDBApi(TMDBNotFoundException ex) {
-        return ResponseEntity.status(404).body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     public record ExceptionValidation(String field, String message, OffsetDateTime timestamp) {
