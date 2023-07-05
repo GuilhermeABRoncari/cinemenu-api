1a3bb99ef30491cad7c99460621717965c4aa302
Guilherme Alves Batista Roncari <123610693+GuilhermeABRoncari@users.noreply.github.com>
2023-07-04 23:08:46 -0300

    [REFACTOR][#41] - Removing internal auto resolver exceptions by spring

--- src/main/java/br/com/cinemenu/cinemenuapi/infra/exceptionhandler/InternalExceptionHandler.java
+++ src/main/java/br/com/cinemenu/cinemenuapi/infra/exceptionhandler/InternalExceptionHandler.java
@@ -5,34 +5,24 @@ import br.com.cinemenu.cinemenuapi.infra.exceptionhandler.exception.InvalidSearc
 import br.com.cinemenu.cinemenuapi.infra.exceptionhandler.exception.TMDBNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.time.OffsetDateTime;
-import java.util.Objects;
 
 @RestControllerAdvice
 public class InternalExceptionHandler {
 
     @ExceptionHandler(InvalidSearchException.class)
-    public ResponseEntity<Object> handleInvalidSearchException(InvalidSearchException ex) {
+    public ResponseEntity<String> handleInvalidSearchException(InvalidSearchException ex) {
         return ResponseEntity.badRequest().body(ex.getMessage());
     }
 
     @ExceptionHandler(InvalidApiKeyException.class)
-    public ResponseEntity<Object> handleInvalidApiKeyException(InvalidApiKeyException ex) {
+    public ResponseEntity<String> handleInvalidApiKeyException(InvalidApiKeyException ex) {
         return ResponseEntity.internalServerError().body(ex.getMessage());
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-        return ResponseEntity.badRequest().body(new ExceptionValidation(String.valueOf(Objects.requireNonNull(ex.getFieldError())), ex.getMessage(), OffsetDateTime.now()));
-    }
-
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<Object> handleInvalidArgumentException(IllegalArgumentException ex) {
+    public ResponseEntity<String> handleInvalidArgumentException(IllegalArgumentException ex) {
         return ResponseEntity.badRequest().body(ex.getMessage());
     }
 
@@ -41,9 +31,4 @@ public class InternalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
-    public record ExceptionValidation(String field, String message, OffsetDateTime timestamp) {
-        public ExceptionValidation(FieldError error) {
-            this(error.getField(), error.getDefaultMessage(), OffsetDateTime.now());
-        }
-    }
 }
