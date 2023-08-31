b960a652a6283d2145b91e07255fbe9e3e1229f1
Guilherme Alves Batista Roncari <123610693+GuilhermeABRoncari@users.noreply.github.com>
2023-08-30 22:39:06 -0300

    [FEATURE][#56] - Optimizes validations

--- src/main/java/br/com/cinemenu/cinemenuapi/domain/dto/requestdto/UserMediaRequestDto.java
+++ src/main/java/br/com/cinemenu/cinemenuapi/domain/dto/requestdto/UserMediaRequestDto.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public record UserMediaRequestDto(
         @Valid
+        @NotNull
         List<UserMediaElementsDto> medias
 ) {
     public record UserMediaElementsDto(
